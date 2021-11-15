076245907eb1d6600cca49d9d8bc939acd2a412e
unknown <cahuzac.p@gmail.com>
2021-11-15 19:58:12 +0100

    first commit

---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+export default function Contact() {
+  return (
+    <div>
+      <span>Me contacter</span>
+      <span>Henry Mickael</span>
+      <span>31100 St Simon</span>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,66 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <Head>
-        <title>Create Next App</title>
+        <title>O2 Piscines</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="flex sm:flex-row md:flex-row justify-center items-center  text-xl bg-gradient-to-r from-blue-500 via-blue-200 to-blue-500 ... w-full h-20 ">
+        <ul>
+          <li className="w-600">
+            <span className="ml-2 mr-2 hover:bg-green-400 hover:text-white-600">
+              Logo
+            </span>
+          </li>
+          <li>
+            <span>
+              <a href="/contact">Contact</a>
+            </span>
+          </li>
+          <li>
+            <span>Réalisations</span>
+          </li>
+        </ul>
+      </div>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main className="flex flex-col items-center justify-center w-full flex-1 sm:w-11/12 md:w-5/6 text-center mt-20">
+        <h1 className="text-3xl font-mono mb-4">Mes services et activités</h1>
+        <span className="mb-12 text-gray-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          doloremque corrupti, repellat quis vitae doloribus voluptatibus
+          voluptate architecto, modi quo quod voluptatem non aperiam minima, aut
+          voluptatum. Iusto, nemo dolor? Dolorem quos nemo dolorum assumenda,
+          vitae itaque et quibusdam. Ipsum ipsa quae blanditiis non dolor harum
+          ratione, beatae iure, facere animi eligendi est asperiores velit qui
+          nam? Ipsum, nostrum ex! At explicabo commodi natus sequi pariatur
+          placeat atque, blanditiis ducimus nulla repellat nemo vel in neque
+          accusamus dolore reprehenderit cumque totam dolorum, nihil odit
+          voluptatibus, exercitationem id similique! Eligendi, corporis.
+        </span>
+        <h2 className="text-3xl font-mono mb-4">Mes partenaires</h2>
+        <span>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          doloremque corrupti, repellat quis vitae doloribus voluptatibus
+          voluptate architecto, modi quo quod voluptatem non aperiam minima, aut
+          voluptatum. Iusto, nemo dolor? Dolorem quos nemo dolorum assumenda,
+          vitae itaque et quibusdam. Ipsum ipsa quae blanditiis non dolor harum
+          ratione, beatae iure, facere animi eligendi est asperiores velit qui
+          nam? Ipsum, nostrum ex! At explicabo commodi natus sequi pariatur
+          placeat atque, blanditiis ducimus nulla repellat nemo vel in neque
+          accusamus dolore reprehenderit cumque totam dolorum, nihil odit
+          voluptatibus, exercitationem id similique! Eligendi, corporis.
+        </span>
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
+      <footer className="flex items-center justify-center w-full h-24 border-t bg-gradient-to-r from-blue-500 via-blue-200 to-blue-500 ...">
+        <span className="my-8 mx-8">Mentions légales</span>{" "}
+        <span className="my-8 mx-8">
+          <a href="/contact">Contact</a>{" "}
+        </span>{" "}
+        <span className="my-8 mx-8">Réalisations</span>
       </footer>
     </div>
-  )
+  );
 }
